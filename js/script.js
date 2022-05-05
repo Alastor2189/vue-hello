3f8c66b3b4b3930602fc3808652d637e1f6a960c
Alastor2189 <g.lucafarci89@gmail.com>
2022-05-05 20:11:49 +0200

    esercizio completato

---
+++ js/script.js
@@ -0,0 +1,10 @@
+Vue.config.devtools = true;
+
+const app = new Vue({
+    el: "#root",
+    data: {
+        jackiePage: 'Hi Jackie',
+        jackieImg: 'https://www.settemuse.it/sfondi_animali/mammiferi/gatti/gatti_026.jpg',
+        imgAlt: "Gatti"
+    }
+});
